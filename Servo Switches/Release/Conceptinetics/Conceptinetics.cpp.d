732378f731c81e32da81f93bf0e39db2d9858665
SetMonkey13 <chriswych@gmail.com>
2018-06-06 21:26:25 -0500

    initial commit

---
+++ Servo Switches/Release/Conceptinetics/Conceptinetics.cpp.d
@@ -0,0 +1,16 @@
+C:\Users\chris\AppData\Local\Temp\VMBuilds\Servo\ Switches\mega_atmega2560\Release\Conceptinetics\Conceptinetics.cpp.o: \
+ C:\Users\chris\Documents\Arduino\libraries\Conceptinetics\Conceptinetics.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h \
+ C:\Users\chris\Documents\Arduino\libraries\Conceptinetics\Conceptinetics.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\chris\Documents\Arduino\libraries\Conceptinetics\Rdm_Uid.h \
+ C:\Users\chris\Documents\Arduino\libraries\Conceptinetics\Rdm_Defines.h
